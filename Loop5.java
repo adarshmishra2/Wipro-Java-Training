962a8df99c9797ac807d16f66c767d42e09070af
Adarsh Mishra <31216840+adarshmishra2@users.noreply.github.com>
2018-09-08 01:48:20 +0530

    Add files via upload

---
+++ Loop5.java
@@ -0,0 +1,11 @@
+import java.util.*;
+class Loop5{
+public static void main(String args[]){
+	for(int i=100; i>=1; i=i-3){
+		if(i < 100){
+		System.out.print(", ");
+		}
+		System.out.print(i);
+	}
+}
+}
