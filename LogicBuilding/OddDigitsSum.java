da69ffe21080095b8394f51e27593ce9ebfb170c
Adarsh Mishra <31216840+adarshmishra2@users.noreply.github.com>
2018-09-10 03:05:49 +0530

    Sum of odd digits in a given integer

---
+++ LogicBuilding/OddDigitsSum.java
@@ -0,0 +1,26 @@
+import java.io.*;
+import  java.util.*;
+
+// Read only region start
+class OddDigitsSum
+{
+
+	public int OddDigitsSum(int input1){
+		// Read only region end
+		// Write code here...
+		int l=String.valueOf(input1).length();
+		int val=input1;
+		int[] arr=new int[l];
+		for(int i=0;i<l;i++){
+			arr[i]=val%10;
+			val=val/10;
+		}
+		int sum=0;
+		for(int i=0;i<arr.length;i++){
+			if(arr[i]%2!=0){
+				sum=sum+arr[i];
+			}
+		}
+		return sum;
+	}
+}
